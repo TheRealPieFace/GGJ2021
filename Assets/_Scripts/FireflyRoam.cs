53066aa16a8b26030432cf7dbc1c2246a652622f
TheRealPieFace <34294484+TheRealPieFace@users.noreply.github.com>
2021-01-28 21:13:26 -0500

    Fireflies Done

---
+++ Assets/_Scripts/FireflyRoam.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireflyRoam : MonoBehaviour
+{
+    public float radius = 3;
+    public float speed = 3;
+    public float jitter = .5f;
+    private Vector3 center;
+    private Vector3 currentDestination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        center = transform.position;
+        PickDestination();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x == currentDestination.x && transform.position.z == currentDestination.z)
+        {
+            PickDestination();
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, currentDestination + new Vector3(0, Random.Range(-jitter, jitter), 0),  Time.deltaTime * speed);
+        }
+    }
+
+    private void PickDestination()
+    {
+        var randomX = Random.Range(center.x - radius, center.x + radius);
+        var randomZ = Random.Range(center.z - radius, center.z + radius);
+        currentDestination = new Vector3(randomX, transform.position.y, randomZ);
+    }
+}
